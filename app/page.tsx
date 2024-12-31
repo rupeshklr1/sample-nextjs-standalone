f90853f2c1c73a5b42904cbdc9c94f7297f13aab
rupeshklr1 <rupeshklr1@gmail.com>
2024-12-31 09:49:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between p-24">
             <div className="container hero">
-               
                 <h1>Greetings from a sample Next.js app!</h1>
-                
-                        <span>Click to deploy:</span>
-                    <span className="block">It will be up in less than 2 minutes!</span>
-        rupesh---->>>pageoooo
+                <span>Click to deploy:</span>
+                <span className="block">It will be up in less than 2 minutes!</span>
+        rupesh---- page from klr krishna --------- leena ----2  sample application runing 
             </div>
         </main>
     );
